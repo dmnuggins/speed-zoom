99387db06d7adecd9e55411aabd5d22cf9bc171b
dylan <7257923+dmnuggins@users.noreply.github.com>
2024-08-17 18:39:01 -0700

    Asteroid style movement and zoom trigger prototype

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -5,6 +5,10 @@ extends Node2D
 
 @export var zoom_speed: float = 2
 
+const DEFAULT_CAM_POS := Vector2(576, 324)
+
+var zoomed: bool = false # flag to track if game is zoomed or not
+
 #Target zoom level
 var target_zoom: Vector2 = Vector2.ONE
 
@@ -14,7 +18,14 @@ func _process(delta: float) -> void:
 
 
 
-func _on_area_2d_body_entered(body):
+func _on_area_2d_body_entered(body: Node) -> void:
 	if body is CharacterBody2D:
-		target_zoom = camera_2d.zoom * 0.8
+		target_zoom = camera_2d.zoom * 3
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body: Node) -> void:
+	if body is CharacterBody2D:
+		target_zoom = Vector2.ONE
+		camera_2d.position 
 	pass # Replace with function body.
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,21 @@
 extends CharacterBody2D
 
-@export var speed = 400
-
+@export var rotation_speed := 5
+@export var speed := 500
+@export var decel := 1.0
+@export var accel := 1.0
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
-	get_input()
+func _process(delta: float) -> void:
+	var rotate = Input.get_axis("left","right") * rotation_speed
+	rotate(rotate * delta)
+
+	
+	if Input.is_action_pressed("up"):
+		velocity = lerp(velocity, transform.y * -speed, accel * delta)
+	
 	move_and_slide()
+	velocity = lerp(velocity, Vector2.ZERO, decel * delta)
